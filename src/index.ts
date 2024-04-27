c14bc20ed0b7cab7a845d1d5ce4d3c5e6934720f
ringoXD <nep360i@gmail.com>
2024-04-27 22:40:06 +0900

    okay done!

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,60 @@ type Env = {
   
 const app = new Hono<{ Bindings: Env }>()
 
+
+const rooturl = 'https://r.a1z.uk'
+
+/*
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.html(`<!DOCTYPE html>
+<html>
+	<title>URL Shortener</title>
+	<body>
+		<h1>Simple URL Shortener</h1>
+		<div class="howto">
+			<h2>How to use?</h2>
+			<ul>
+				<li>(作成)<code>/api/links</code>にPOSTする</li>
+				<p>リクエスト、レスポンスの内容は共にjson形式です。</p>
+				<p><code>{"url": "https://example.com"}</code>のようにしてPOSTしてください。</p>
+
+				<li>(使用)<code>/短縮リンク</code>にアクセスする</li>
+				<p>生成した短縮リンクにアクセスすると自動でリダイレクトされます。</p>
+			</ul>
+		</div>
+	</body>
+</html>
+
+<style>
+	html {
+		background-color: antiquewhite;
+	}
+</style>
+`)
+})
+*/
+
+app.get('/', (c) => {
+    return c.html(`
+        <html>
+            <h1>URL短縮サービス</h1>
+            <div>
+                <span>短縮したいURLを入力してください</span>
+            </div>
+            <form action='/shorten' method='post'>
+                <div>
+                    <label>URL</label>
+                    <input type='url' name='url' required />
+                    <button>短縮</button>
+                </div>
+            </form>
+        </html>
+    `)
+})
+
+
+app.get('/favicon.ico', async (c) => {
+	return c.text('無理', 404)
 })
 
 async function shorten(kv: KVNamespace, url: string) { //TODO: fix TypeError: Cannot read properties of undefined (reading 'put')
@@ -23,9 +75,30 @@ async function shorten(kv: KVNamespace, url: string) { //TODO: fix TypeError: Ca
 	await kv.put(key, JSON.stringify({ url, createdAt }))
 	return { key }
 }
-  
+
+
+app.post('/shorten', async (c) => {
+	const body = await c.req.parseBody<{url: string}>()
+	console.log(body)
+	const { url } = body
+	// const { url } = await c.req.json<{ url: string }>()
+	if (!url || typeof url !="string") {
+		return c.html('<p>無効なURLが入力されました</p>', 400)
+	}
+	const { key } = await shorten(c.env.SHORT_URLS, url)
+	// return c.json({ key, url })
+	return c.html(`
+	<h1>生成しました!</h1>
+	<p>${rooturl}/${key} - リダイレクト先: ${url}</p>
+	`)
+})
+
+app.get('/shorten', async (c) => {
+	return c.text('どこ見てんの?')
+})
+
 app.post('/api/links', async (c) => {
-	const body = await c.req.parseBody()
+	const body = await c.req.parseBody<{url: string}>()
 	console.log(body)
 	const { url } = body
 	// const { url } = await c.req.json<{ url: string }>()
@@ -46,6 +119,16 @@ interface URL {
 async function getUrl(kv: KVNamespace, key: string) {
 	return kv.get<URL>(key, 'json')
 }
+
+app.get('/:key', async (c) => {
+	const key = c.req.param('key')
+	const res = await getUrl(c.env.SHORT_URLS, key)
+	if (!res) {
+		return c.text('存在しない短縮リンクです。', 404)
+	}
+	const url = res.url
+	return c.redirect(url, 301)
+})
   
 app.get('/api/links/:key', async (c) => {
 	const key = c.req.param('key')
